00175fd5d912f4e5ffe4972c24e166127eccbcfd
2004sirojbek <kmamurjon0@gmail.com>
2024-04-18 17:41:31 +0500

    server

--- index.js
+++ index.js
@@ -6,7 +6,15 @@ const  hostname = '172.20.4.233';
 const port = 3001;
 
 const server = http.createServer((req, res) =>{
-const filePath =path.join(__dirname, 'index.html');
+let filePath =path.join(__dirname, 'index.html');
+
+if(req.url==='/'){
+       filePath = path.join(__dirname, 'index.html');
+} else if (req.url === '/about'){
+       filePath=path.join(__dirname, 'about.html');
+}else if (req.url==='/detculs'){
+       filePath=path.join(__dirname, 'detculs.html');
+}
 
     fs.readFile(filePath, (err, data) =>{
        if(err){
@@ -21,22 +29,6 @@ const filePath =path.join(__dirname, 'index.html');
 
     });
 });
-const server1 = http.createServer((req, res) =>{
-       const filePath =path.join(__dirname, 'abeut.html');
-       
-           fs.readFile(filePath, (err, data) =>{
-              if(err){
-                     res.writeHead(500)
-                     res.end("Error loading the file: "+err.code)
-       
-              }
-              else{
-                     res.writeHead(200, { 'Content-Type': 'text/html'});
-                     res.end(data, 'utf-8');
-              }
-       
-           });
-       });
        
 server.listen(port, hostname, () =>{
        console.log(`Server runing at http://${hostname}:${port}/`);
