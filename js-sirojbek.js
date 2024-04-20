19cf7fe0d8c7539464a5f29ae9c1a68449bc663e
2004sirojbek <kmamurjon0@gmail.com>
2024-04-20 17:11:02 +0500

    server3

---
+++ js-sirojbek.js
@@ -0,0 +1,38 @@
+{
+    let a=0;   
+    console.log(a);
+}
+// this is single line comment
+
+/*- this is mulit line comment
+
+*/
+console.log("this is log print massage");
+
+// variebles, var, let, const
+
+var myVar = 'this is string var'
+var myNum = 123 // this is number var
+var myBoolean = true // this is boolean var
+
+//{
+let myVarUsinglet="this is string 5656";
+const myConst = 112;
+console.log("myVarUsinglet");
+console.log("my Const");
+//}
+{
+var thisVarInsideScope = "this is5678";
+}
+console.log("6888888");
+
+let myboole1 = 0 // false,1,0
+
+if(myboole1)
+{
+console.log("this is cindition is true");
+}
+else
+{
+    console.log("this is cindition is false");
+}
